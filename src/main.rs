0dca924a75c02faecced85af29e3038f687b07e9
jch <julianchiu97@gmail.com>
2026-04-09 17:06:48 +1000

    feat: implemented statistics for base counts

--- src/main.rs
+++ src/main.rs
@@ -50,13 +50,21 @@ fn vcf_reader(file_path: &str) -> Result<VecDeque<Variant>, Box<dyn Error>> {
 	Ok(variants)
 }
 
+#[derive(Debug, Clone, Copy)]
+struct BaseCountsStats {
+    depth: u32,
+    refr_count: u32,
+    alt_count: u32,
+    alt_vaf: f64,
+}
+
 #[derive(Debug, Clone, Default)]
 struct BaseCounts {
-    a: u64,
-    c: u64,
-    g: u64,
-    t: u64,
-    n: u64,
+    a: u32,
+    c: u32,
+    g: u32,
+    t: u32,
+    n: u32,
 }
 
 impl BaseCounts {
@@ -68,6 +76,39 @@ impl BaseCounts {
             'T' => self.t += 1,
             'N' => self.n += 1,
             _ => eprintln!("Warning: Base does not match: {}", base),
+        }
+    }
+
+    fn count_for_base(&self, base: char) -> u32 {
+        match base {
+            'A' => self.a,
+            'C' => self.c,
+            'G' => self.g,
+            'T' => self.t,
+            'N' => self.n,
+            _ => 0,
+        }
+    }
+
+    fn depth(&self) -> u32 {
+        self.a + self.c + self.g + self.t + self.n
+    }
+
+    fn stats(&self, refr: char, alt: char) -> BaseCountsStats {
+        let depth = self.depth();
+        let refr_count = self.count_for_base(refr);
+        let alt_count = self.count_for_base(alt);
+        let alt_vaf = if depth > 0 {
+            alt_count as f64 / depth as f64
+        } else {
+            0.0
+        };
+
+        BaseCountsStats {
+            depth,
+            refr_count,
+            alt_count,
+            alt_vaf,
         }
     }
 }
@@ -82,10 +123,18 @@ struct Variant {
 	counts: BaseCounts,
 }
 
+impl Variant {
+    fn base_counts_stats(&self) -> Option<BaseCountsStats> {
+        let refr_char = self.refr.chars().next()?;
+        let alt_char = self.alt.chars().next()?;
+        Some(self.counts.stats(refr_char, alt_char))
+    }
+}
+
 fn get_vcf_min_max(variants: &VecDeque<Variant>) -> Option<(String, u64, u64)> {
     let first = variants.front()?;
     let chrom = first.chrom.clone();
-    
+
     let min_pos = first.pos;
     let max_pos = variants.back()?.pos;
 
@@ -136,7 +185,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         get_vcf_min_max(&variants).ok_or("Could not determine VCF min/max")?;
 
     println!("Region Chromosome: {}, Min Pos: {}, Max Pos: {}", region_chrom, min_pos, max_pos);
-    println!("{:?}", variants);
+    
+    println!("All Variants in the Variants VecDeque:");
+    for variant in &variants {
+        println!("{:?}", variant);
+        let base_counts_stats = variant.base_counts_stats();
+        println!("{:?}", base_counts_stats);
+    }
 
     Ok(())
 }
