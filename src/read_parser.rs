00e989f8cb7005e7e0d2023ee6ed8a0b853c3229
jch <julianchiu97@gmail.com>
2026-07-26 18:34:25 +1000

    refactor: moved opening reader and processing bin into own function

--- src/read_parser.rs
+++ src/read_parser.rs
@@ -1,15 +1,16 @@
 use std::collections::VecDeque;
+use std::fs::File;
 use std::path::Path;
 use std::rc::Rc;
 
 use anyhow::{Context, Result};
-use csv::{WriterBuilder};
-use rust_htslib::bam::{Read, IndexedReader, Record};
+use csv::{Writer, WriterBuilder};
+use rust_htslib::bam::{IndexedReader, Read, Record};
 use tracing::{debug, info};
 
 use crate::errors::AppError;
 use crate::output::{write_variant_row, write_header};
-use crate::variant::{Variant, VarClass, BinnedVariants};
+use crate::variant::{BinnedVariants, VarClass, Variant, VariantBin};
 
 pub fn parse_region(
     binned_variants: &mut BinnedVariants,
@@ -21,12 +22,114 @@ pub fn parse_region(
     fasta_path: Option<&Path>,
     // ADD vector of [chrom/first variant index/length] here
 ) -> Result<()> {
-    let file_type = detect_file_type(reads_path)?;
+    let mut reader = open_indexed_reader(reads_path, fasta_path)?;
+
+    // let file_type = detect_file_type(reads_path)?;
     
     let mut csv_writer = WriterBuilder::new()
         .has_headers(false)
         .from_path(csv_path)
         .with_context(|| format!("Failed to create output CSV '{}'", csv_path.display()))?;
+
+    // let mut reader = IndexedReader::from_path(reads_path)
+    //     .with_context(|| format!("Failed to open reads file '{}'", reads_path.display()))?;
+
+    // match file_type {
+    //     FileType::Bam => {}
+    //     FileType::Cram => {
+    //         let fasta = fasta_path.ok_or(AppError::MissingCramReference)?;
+
+    //         reader
+    //             .set_reference(fasta)
+    //             .with_context(|| format!("Failed to set CRAM reference to '{}'", fasta.display()))?;
+    //     },
+    // };
+
+    // Write dynamic header based on BAM/CRAM filename
+    write_header(&mut csv_writer, reads_path, label, varclass)
+        .context("Could not CSV write header")?;
+
+    for bin in &mut binned_variants.bins{
+
+        process_bin(bin, &mut reader, &mut csv_writer, sample)?;
+
+        // let chrom_info = get_chrom_info(&bin.variants);
+
+        // reader.fetch((&bin.chrom, chrom_info.min_pos - 1, chrom_info.max_pos))
+        //     .with_context(|| 
+        //         format!(
+        //             "Failed to fetch region {}:{}-{}",
+        //                 bin.chrom,
+        //                 chrom_info.min_pos,
+        //                 chrom_info.max_pos,    
+        //         )
+        //     )?;
+
+        // debug!(
+        //     "Processing bin from chromosome: {} \n
+        //      Spanning position {} - {} \n
+        //      Number of variants being processed {}",
+        //     bin.chrom,
+        //     chrom_info.min_pos,
+        //     chrom_info.max_pos,
+        //     bin.variants.len(),
+        // );
+
+        // let ref_seq_len = get_ref_len(&reader, &bin.chrom)?;
+
+        // for read_result in reader.rc_records() {
+        //     let record = read_result
+        //         .context("Failed getting read from reads file")?;
+            
+        //     if skip_read_check(&record) {
+        //         continue;
+        //     }
+
+        //     let read_start = record.pos() as u64;
+
+        //     while let Some(var) = bin.variants.front() {
+        //         if (read_start + 1) > var.info.pos {
+        //                 let pos_normalized: f64 = var.get_pos_normalized(ref_seq_len);
+        //                 write_variant_row(&mut csv_writer, var, pos_normalized, sample)?;
+        //                 bin.variants.pop_front();
+
+        //         } else {
+        //             break;
+        //         }
+        //     }
+
+        //     for var in &mut bin.variants {
+        //         let zero_based_pos = var.info.pos - 1;
+        //         let read_end = record.cigar().end_pos() as u64;
+
+        //         if zero_based_pos >= read_start && zero_based_pos < read_end {
+        //             var.count_locus_features(&record, zero_based_pos);
+        //         } else {
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // while let Some(var) = bin.variants.pop_front() {
+        //     let pos_normalized = var.get_pos_normalized(ref_seq_len);
+        //     write_variant_row(&mut csv_writer, &var, pos_normalized, sample)?;
+        // }
+        csv_writer
+            .flush()
+            .context("Failed to flush output CSV to disk")?;
+    }
+
+    info!("Parsed reads successfully");
+    info!("Csv output can be found at: {:?}", csv_path.display());
+
+    Ok(())
+}
+
+fn open_indexed_reader(
+    reads_path: &Path,
+    fasta_path: Option<&Path>,
+) -> Result<IndexedReader> {
+    let file_type = detect_file_type(reads_path)?;
 
     let mut reader = IndexedReader::from_path(reads_path)
         .with_context(|| format!("Failed to open reads file '{}'", reads_path.display()))?;
@@ -35,16 +138,11 @@ pub fn parse_region(
         FileType::Bam => {}
         FileType::Cram => {
             let fasta = fasta_path.ok_or(AppError::MissingCramReference)?;
-
             reader
                 .set_reference(fasta)
                 .with_context(|| format!("Failed to set CRAM reference to '{}'", fasta.display()))?;
-        },
-    };
-
-    // Write dynamic header based on BAM/CRAM filename
-    write_header(&mut csv_writer, reads_path, label, varclass)
-        .context("Could not CSV write header")?;
+        }
+    }
 
     info!(
         "Parsing reads from {:?} as: {:?}",
@@ -52,76 +150,72 @@ pub fn parse_region(
         file_type
     );
 
-    for bin in &mut binned_variants.bins{
+    Ok(reader)
+}
 
-        let chrom_info = get_chrom_info(&bin.variants);
+fn process_bin(
+    bin: &mut VariantBin,
+    reader: &mut IndexedReader,
+    csv_writer: &mut Writer<File>,
+    sample: Option<&str>,
+) -> Result<()> {
+    let chrom_info = get_chrom_info(&bin.variants);
 
-        reader.fetch((&bin.chrom, chrom_info.min_pos - 1, chrom_info.max_pos))
-            .with_context(|| 
-                format!(
-                    "Failed to fetch region {}:{}-{}",
-                        bin.chrom,
-                        chrom_info.min_pos,
-                        chrom_info.max_pos,    
-                )
-            )?;
+    reader.fetch((&bin.chrom, chrom_info.min_pos - 1, chrom_info.max_pos))
+        .with_context(|| {
+            format!(
+                "Failed to fetch region {}:{}-{}",
+                bin.chrom,
+                chrom_info.min_pos,
+                chrom_info.max_pos,
+            )
+        })?;
 
-        debug!(
-            "Processing bin from chromosome: {} \n
-             Spanning position {} - {} \n
-             Number of variants being processed {}",
-            bin.chrom,
-            chrom_info.min_pos,
-            chrom_info.max_pos,
-            bin.variants.len(),
-        );
+    debug!(
+        "Processing bin from chromosome: {} spanning {}-{}, number of variants {}",
+        bin.chrom,
+        chrom_info.min_pos,
+        chrom_info.max_pos,
+        bin.variants.len(),
+    );
 
-        let ref_seq_len = get_ref_len(&reader, &bin.chrom)?;
+    let ref_seq_len = get_ref_len(reader, &bin.chrom)?;
 
-        for read_result in reader.rc_records() {
-            let record = read_result
-                .context("Failed getting read from reads file")?;
-            
-            if skip_read_check(&record) {
-                continue;
-            }
+    for read_result in reader.rc_records() {
+        let record = read_result.context("Failed getting read from reads file")?;
 
-            let read_start = record.pos() as u64;
+        if skip_read_check(&record) {
+            continue;
+        }
 
-            while let Some(var) = bin.variants.front() {
-                if (read_start + 1) > var.info.pos {
-                        let pos_normalized: f64 = var.get_pos_normalized(ref_seq_len);
-                        write_variant_row(&mut csv_writer, var, pos_normalized, sample)?;
-                        bin.variants.pop_front();
+        let read_start = record.pos() as u64;
+        let read_end = record.cigar().end_pos() as u64;
 
-                } else {
-                    break;
-                }
-            }
-
-            for var in &mut bin.variants {
-                let zero_based_pos = var.info.pos - 1;
-                let read_end = record.cigar().end_pos() as u64;
-
-                if zero_based_pos >= read_start && zero_based_pos < read_end {
-                    var.count_locus_features(&record, zero_based_pos);
-                } else {
-                    break;
-                }
+        while let Some(var) = bin.variants.front() {
+            if (read_start + 1) > var.info.pos {
+                let pos_normalized = var.get_pos_normalized(ref_seq_len);
+                write_variant_row(csv_writer, var, pos_normalized, sample)?;
+                bin.variants.pop_front();
+            } else {
+                break;
             }
         }
 
-        while let Some(var) = bin.variants.pop_front() {
-            let pos_normalized = var.get_pos_normalized(ref_seq_len);
-            write_variant_row(&mut csv_writer, &var, pos_normalized, sample)?;
+        for var in &mut bin.variants {
+            let zero_based_pos = var.info.pos - 1;
+
+            if zero_based_pos >= read_start && zero_based_pos < read_end {
+                var.count_locus_features(&record, zero_based_pos);
+            } else {
+                break;
+            }
         }
-        csv_writer
-            .flush()
-            .context("Failed to flush output CSV to disk")?;
     }
 
-    info!("Parsed reads successfully");
-    info!("Csv output can be found at: {:?}", csv_path.display());
+    while let Some(var) = bin.variants.pop_front() {
+        let pos_normalized = var.get_pos_normalized(ref_seq_len);
+        write_variant_row(csv_writer, &var, pos_normalized, sample)?;
+    }
 
     Ok(())
 }
