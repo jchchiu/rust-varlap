f3d35ac0980869754cad60d50d71dd22b758003e
jch <julianchiu97@gmail.com>
2026-09-10 17:12:03 +1000

    chore: added spanning for logging multithread bam parsing

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod variant_parser;
 
 use anyhow::Result;
 use rayon::prelude::*;
-use tracing::info;
+use tracing::{info, info_span};
 
 use crate::errors::AppError;
 
@@ -27,28 +27,31 @@ fn run() -> Result<()> {
 
     // Initialize the thread pool for analyzing multiple BAM files in parallel
     rayon::ThreadPoolBuilder::new().num_threads(args.threads).build_global()?;
-
+  
     args.reads
         .par_iter()
         .enumerate()
         .try_for_each(|(i, reads)| -> Result<()> {
-            info!("-------------------------------------------------------------------------------");
+            let span = info_span!("sample", index = i, path = %reads.display());
+            span.in_scope(|| -> Result<()> {
+                info!("-------------------------------------------------------------------------------");
 
-            let label = args.label.get(i).and_then(|l| l.as_deref());
+                let label = args.label.get(i).and_then(|l| l.as_deref());
 
-            let mut binned_variants = binning::bin(&parsed_variants, args.gap)?;
+                let mut binned_variants = binning::bin(&parsed_variants, args.gap)?;
 
-            read_parser::parse(
-                &mut binned_variants,
-                reads,
-                &output_paths[i],
-                args.sample.as_deref(),
-                label,
-                &args.varclass,
-                args.fasta.as_deref(),
-            )?;
+                read_parser::parse(
+                    &mut binned_variants,
+                    reads,
+                    &output_paths[i],
+                    args.sample.as_deref(),
+                    label,
+                    &args.varclass,
+                    args.fasta.as_deref(),
+                )?;
 
-            Ok(())
+                Ok(())
+            })
         })?;
 
     if args.merge {
